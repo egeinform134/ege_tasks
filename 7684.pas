d192368d5fa5370878d4c55bd92351111956dacd
egeinform134 <egeinform134@leeching.net>
2016-04-10 11:04:10 +0400

    Create 7684.pas

---
+++ 7684.pas
@@ -0,0 +1,28 @@
+program sevensixeightfour;
+var max_odd,max_even,current,amount,incontrol,i:integer;
+begin
+	max_odd:=0;
+	max_even:=0;
+	current:=0;
+	amount:=0;
+	i:=0;
+	incontrol:=0;
+	readln(amount);
+	for i:=1 to amount do
+	begin
+		readln(current);
+		if ( current mod 2 = 0 ) then begin
+			if current > max_even then
+				max_even:=current
+			end
+		else 
+			if current > max_odd then
+				max_odd:=current;
+	end;
+	readln(incontrol);
+	writeln('Вычисленное контрольное значение: ', max_odd + max_even);
+	if incontrol = max_odd + max_even then
+		writeln('Контроль пройден')
+	else
+		writeln('Контроль не пройден')
+end.
