d7dc9d2583eae539a601ba1f52a93f40bad60498
Dmitry Luschan <dluschan@gmail.com>
2016-04-11 17:11:42 +0300

    задача 3109 готова

--- 3109.pas
+++ 3109.pas
@@ -1,12 +1,23 @@
 program egr;
+type
+	result = record
+		res: integer;
+		count: integer;
+		name: string;
+	end;
 var
-    amount, couples, i, j: integer;
+	amount, sum, i: integer;
     name, surname: string;
     c: char;
     mark: array[1..3] of integer;
-    fails: array[1..3] of string;
+    fails: array[1..3] of result;
 begin
     readln(amount);
+	
+	fails[1].res := 16;
+	fails[2].res := 16;
+	fails[3].res := 16;
+	
     for i := 1 to amount do
     begin
         name    := '';
@@ -25,15 +36,52 @@ begin
         delete(surname, length(surname), 1);
 
         readln(mark[1], mark[2], mark[3]);
+		sum := mark[1] + mark[2] + mark[3];
 
-        couples := 0;
-        for j := 1 to 3 do
-            if mark[j] = 2 then
-                couples := couples + 1;
+		if (sum = fails[3].res) then
+		begin
+			fails[3].count := fails[3].count + 1;
+			fails[3].name := fails[3].name + ' ' + surname + ' ' + name + ';';
+		end;
+		if (sum < fails[3].res) and (sum > fails[2].res) then
+		begin
+			fails[3].res := sum;
+			fails[3].count := 1;
+			fails[3].name := surname + ' ' + name + ';';
+		end;
+		if (sum = fails[2].res) then
+		begin
+			fails[2].count := fails[2].count + 1;
+			fails[2].name := fails[2].name + ' ' + surname + ' ' + name + ';';
+		end;
+		if (sum > fails[1].res) and (sum < fails[2].res) then
+		begin
+			fails[3] := fails[2];
 
-        if couples > 0 then
-            fails[couples] := fails[couples] + surname + ' ' + name + '; ';
+			fails[2].res := sum;
+			fails[2].count := 1;
+			fails[2].name := surname + ' ' + name + ';';
+		end;
+		if (sum = fails[1].res) then
+		begin
+			fails[1].count := fails[1].count + 1;
+			fails[1].name := fails[1].name + ' ' + surname + ' ' + name + ';';
+		end;
+		if (sum < fails[1].res) then
+		begin
+			fails[3] := fails[2];
+			fails[2] := fails[1];
+
+			fails[1].res := sum;
+			fails[1].count := 1;
+			fails[1].name := surname + ' ' + name + ';';
+		end; 
     end;
-    for j := 3 downto 1 do
-        writeln(fails[j]);
+	sum:=0;
+	i:=1;
+	repeat
+		sum := sum + fails[i].count;
+		writeln(fails[i].name);
+		i := i + 1;
+	until sum >= 3;
 end.
