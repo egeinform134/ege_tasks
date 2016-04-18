a6ad329b031a8ef2a0ac87bfcbbd2e2f948c023a
Dmitry Luschan <dluschan@gmail.com>
2016-04-18 16:24:39 +0300

    новая задача

---
+++ 5226.pas
@@ -0,0 +1,14 @@
+program manymax;
+var
+	c:char;
+	name,surname:string;
+	amount,prize,schoolnum,mark,i,firstresult,secondresult,winners,secondwinners:integer;
+	firstns,secondns:string;
+begin
+	readln(amount, prize);
+
+	for i := 1 to amount do
+	begin
+		
+	end;
+end.
