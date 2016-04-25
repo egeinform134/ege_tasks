92c724fb917a02b1a7059fd1303c7e5486cfe180
Dmitry Luschan <dluschan@gmail.com>
2016-04-25 17:01:46 +0300

    commit

--- 7772.pas
+++ 7772.pas
@@ -1,23 +1,25 @@
 program sevensevenseventwo;
+const
+	n = 8;
 var
 	amount, i, max_tail, max, number: integer;
-	buffer: array [0..7] of integer;
+	buffer: array [0..n-1] of integer;
 begin
 	readln(amount);
-	for i := 0 to 7 do
+	for i := 0 to n-1 do
 		readln(buffer[i]);
 		
 	max := -1;
 	max_tail := buffer[0];
 	
-	for i := 8 to amount - 1 do
+	for i := n to amount - 1 do
 	begin
 		readln(number);
-		if buffer[i mod 8] > max_tail then
-			max_tail := buffer[i mod 8];
+		if buffer[i mod n] > max_tail then
+			max_tail := buffer[i mod n];
 		if number * max_tail > max then
 			max := number * max_tail;
-		buffer[i mod 8] := number;
+		buffer[i mod n] := number;
 	end;
 	writeln(max);
 end.
