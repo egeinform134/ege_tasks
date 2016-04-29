0c0e146a6682f06c0277158ff37a9a0f43acc04c
egeinform134 <egeinform134@leeching.net>
2016-04-29 10:05:50 +0300

    Решена задача 5258, добавлены входные данные для неё

--- 5258.pas
+++ 5258.pas
@@ -1,7 +1,17 @@
 program fivetwofiveeight;
 var
-	
+	amount, x, y, i, min_current, min_range, quarter_max, quarter_max_num: integer;
+	quarter, min: array [1..4] of integer;
+	min_point: array [1..4] of string;
+	x_string, y_string, min_point_total: string;
 begin
+
+	for i := 1 to 4 do
+	begin
+		min[i] := -1;
+		quarter[i] := 0;
+	end;
+
 	readln(amount);
 	for i := 1 to amount do
 	begin
@@ -15,10 +25,20 @@ begin
 			else 
 				min_current := abs(y);
 			if min[1] = -1 then
-				min[1] := min_current
+			begin
+				min[1] := min_current;
+				str(x,x_string);
+				str(y,y_string);
+				min_point[1] := x_string + ' ' + y_string;
+			end
 			else
 				if min_current < min[1] then
+				begin
 					min[1] := min_current;
+					str(x,x_string);
+					str(y,y_string);
+					min_point[1] := x_string + ' ' + y_string;
+				end;
 		end;
 
 		if ( x > 0 ) and ( y < 0 ) then
@@ -29,10 +49,20 @@ begin
 			else
 				min_current := abs(y);
 			if min[2] = -1 then
-				min[2] := min_current
+			begin
+				min[2] := min_current;
+				str(x,x_string);
+				str(y,y_string);
+				min_point[2] := x_string + ' ' + y_string;
+			end
 			else
 				if min_current < min[2] then
+				begin
 					min[2] := min_current;
+					str(x,x_string);
+					str(y,y_string);
+					min_point[2] := x_string + ' ' + y_string;
+				end;
 		end;
 
 		if ( x < 0 ) and ( y < 0 ) then
@@ -42,11 +72,21 @@ begin
 				min_current := abs(x)
 			else
 				min_current := abs(y);
-			if min[3] := -1 then
-				min[3] := min_current
+			if min[3] = -1 then
+			begin
+				min[3] := min_current;
+				str(x,x_string);
+				str(y,y_string);
+				min_point[3] := x_string + ' ' + y_string;
+			end
 			else
 				if min_current < min[3] then
+				begin
 					min[3] := min_current;
+					str(x,x_string);
+					str(y,y_string);
+					min_point[3] := x_string + ' ' + y_string;
+				end;
 		end;
 
 		if ( x < 0 ) and ( y > 0 ) then
@@ -56,23 +96,48 @@ begin
 				min_current := abs(x)
 			else
 				min_current := abs(y);
-			if min[4] := -1 then
+			if min[4] = -1 then
+			begin
 				min[4] := min_current;
+				str(x,x_string);
+				str(y,y_string);
+				min_point[4] := x_string + ' ' + y_string;
+			end
 			else
 				if min_current < min[4] then
+				begin
 					min[4] := min_current;
+					str(x,x_string);
+					str(y,y_string);
+					min_point[4] := x_string + ' ' + y_string;
+				end;
 		end;
 	end;
+
+	quarter_max := -1;
+
 	for i := 1 to 4 do
+	begin
 		if quarter[i] > quarter_max then
 		begin
+			quarter_max_num := i;
 			quarter_max := quarter[i];
 			min_range := min[i];
+			min_point_total := min_point[i];
 		end
 		else
 			if quarter[i] = quarter_max then
 			begin
-				if quarter
-			end
+				if min[i] < min[quarter_max_num] then
+				begin
+					min_range := min[i];
+					quarter_max := quarter[i];
+					min_point_total := min_point[i];
+					quarter_max_num := i;
+				end;
+			end;
+	end;
+
+	writeln('В четверти ', quarter_max_num, ' есть ', quarter_max, ' точек, минимальное расстояние ', min_range, ' у точки (', min_point_total, ')');
 
 end.
