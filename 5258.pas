8a89daa4f4b85ad25ef6faf2b6b3df6338d1097d
Dmitry Luschan <dluschan@gmail.com>
2016-05-02 14:19:29 +0300

    оптимизация 5258

--- 5258.pas
+++ 5258.pas
@@ -4,6 +4,24 @@ var
 	quarter, min: array [1..4] of integer;
 	min_point: array [1..4] of string;
 	x_string, y_string, min_point_total: string;
+
+function fun(n: integer; x, y: integer): integer;
+begin
+	quarter[n] := quarter[n] + 1;
+	if abs(x) < abs(y) then
+		min_current := abs(x)
+	else
+		min_current := abs(y);
+
+	if (min[n] = -1) or (min_current < min[n]) then
+	begin
+		min[n] := min_current;
+		str(x,x_string);
+		str(y,y_string);
+		min_point[n] := x_string + ' ' + y_string;
+	end
+end;
+
 begin
 
 	for i := 1 to 4 do
@@ -15,128 +33,32 @@ begin
 	readln(amount);
 	for i := 1 to amount do
 	begin
-		readln(x,y);
-
+		readln(x, y);
+				
 		if ( x > 0 ) and ( y > 0 ) then
-		begin
-			quarter[1] := quarter[1] + 1;
-			if abs(x) < abs(y) then
-				min_current := abs(x)
-			else 
-				min_current := abs(y);
-			if min[1] = -1 then
-			begin
-				min[1] := min_current;
-				str(x,x_string);
-				str(y,y_string);
-				min_point[1] := x_string + ' ' + y_string;
-			end
-			else
-				if min_current < min[1] then
-				begin
-					min[1] := min_current;
-					str(x,x_string);
-					str(y,y_string);
-					min_point[1] := x_string + ' ' + y_string;
-				end;
-		end;
+			fun(1, x, y);
 
 		if ( x > 0 ) and ( y < 0 ) then
-		begin
-			quarter[2] := quarter[2] + 1;
-			if abs(x) < abs(y) then
-				min_current := abs(x)
-			else
-				min_current := abs(y);
-			if min[2] = -1 then
-			begin
-				min[2] := min_current;
-				str(x,x_string);
-				str(y,y_string);
-				min_point[2] := x_string + ' ' + y_string;
-			end
-			else
-				if min_current < min[2] then
-				begin
-					min[2] := min_current;
-					str(x,x_string);
-					str(y,y_string);
-					min_point[2] := x_string + ' ' + y_string;
-				end;
-		end;
+			fun(2, x, y);
 
 		if ( x < 0 ) and ( y < 0 ) then
-		begin
-			quarter[3] := quarter[3] + 1;
-			if abs(x) < abs(y) then
-				min_current := abs(x)
-			else
-				min_current := abs(y);
-			if min[3] = -1 then
-			begin
-				min[3] := min_current;
-				str(x,x_string);
-				str(y,y_string);
-				min_point[3] := x_string + ' ' + y_string;
-			end
-			else
-				if min_current < min[3] then
-				begin
-					min[3] := min_current;
-					str(x,x_string);
-					str(y,y_string);
-					min_point[3] := x_string + ' ' + y_string;
-				end;
-		end;
-
+			fun(3, x, y);
+			
 		if ( x < 0 ) and ( y > 0 ) then
-		begin
-			quarter[4] := quarter[4] + 1;
-			if abs(x) < abs(y) then
-				min_current := abs(x)
-			else
-				min_current := abs(y);
-			if min[4] = -1 then
-			begin
-				min[4] := min_current;
-				str(x,x_string);
-				str(y,y_string);
-				min_point[4] := x_string + ' ' + y_string;
-			end
-			else
-				if min_current < min[4] then
-				begin
-					min[4] := min_current;
-					str(x,x_string);
-					str(y,y_string);
-					min_point[4] := x_string + ' ' + y_string;
-				end;
-		end;
+			fun(4, x, y);
 	end;
 
 	quarter_max := -1;
 
 	for i := 1 to 4 do
 	begin
-		if quarter[i] > quarter_max then
+		if (quarter[i] > quarter_max) or ((quarter[i] = quarter_max) and (min[i] < min[quarter_max_num])) then
 		begin
 			quarter_max_num := i;
 			quarter_max := quarter[i];
 			min_range := min[i];
 			min_point_total := min_point[i];
-		end
-		else
-			if quarter[i] = quarter_max then
-			begin
-				if min[i] < min[quarter_max_num] then
-				begin
-					min_range := min[i];
-					quarter_max := quarter[i];
-					min_point_total := min_point[i];
-					quarter_max_num := i;
-				end;
-			end;
-	end;
+		end;
 
 	writeln('В четверти ', quarter_max_num, ' есть ', quarter_max, ' точек, минимальное расстояние ', min_range, ' у точки (', min_point_total, ')');
 
