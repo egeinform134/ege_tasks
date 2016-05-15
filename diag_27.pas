00a79a22ab96b689b2b3bb4e4118d7bad807f7cd
Dmitry Luschan <dluschan@gmail.com>
2016-05-15 13:34:05 +0300

    diag_27

--- diag_27.pas
+++ diag_27.pas
@@ -1,6 +1,6 @@
 program diag_27;
 var
-	amount, i, x, y, triangles: integer;
+        amount, i, x, y: integer;
 	quarter: array [1..4] of integer;
 begin
 	readln(amount);
@@ -12,13 +12,12 @@ begin
 		readln(x,y);
 		if ( x > 0 ) and ( y > 0 ) then
 			quarter[1] := quarter[1] + 1;
-		if ( x > 0 ) and ( y < 0 ) then
-			quarter[2] := quarter[2] + 1;
+                if ( x < 0 ) and ( y > 0 ) then
+                        quarter[2] := quarter[2] + 1;
 		if ( x < 0 ) and ( y < 0 ) then
 			quarter[3] := quarter[3] + 1;
-		if ( x < 0 ) and ( y > 0 ) then
+                if ( x > 0 ) and ( y < 0 ) then
 			quarter[4] := quarter[4] + 1;
 	end;
-	triangles := (quarter[1] * (abs(quarter[1] - 1)) * quarter[4] + quarter[4] * (abs(quarter[4] - 1)) * quarter[1] + quarter[2] * (abs(quarter[2] - 1)) * quarter[3] + quarter[3] * (abs(quarter[3] - 1)) * quarter[2]);
-	writeln(triangles / 2);
+        writeln((quarter[1]*quarter[4]*(quarter[1]+quarter[4]-2)+quarter[2]*quarter[3]*(quarter[2]+quarter[3]-2)) div 2);
 end.
