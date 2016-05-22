996eef65cf4cdce9cb5d3e8c66654d6c63fffffd
Dmitry Luschan <dluschan@gmail.com>
2016-05-22 14:07:03 +0300

    lesenka

--- ladder.pas
+++ ladder.pas
@@ -1,30 +1,31 @@
 program ladder;
 var
-	i, floors, blocks, answer: integer;	
+        blocks: integer;
 
-function crladder(x: integer): integer;
+function crladder(x, previous: integer): integer;
+var
+        currentfloor, floors: integer;
 begin
-	writeln('Функция вызвана для x = ', x);
-	if x = 1 then
-	begin
-		crladder := 1;
-		writeln('x = 1, crladder = 1');
-	end
-	else
-	begin
-		for i := x - 1 downto 1 do
-		begin
-			writeln('i = ', i);
-			floors := floors + crladder(i); 
-		end;
-		crladder := floors;
-	end;
+        floors := 0;
+
+        if x > previous*(previous-1) div 2 then
+            crladder := 0
+        else
+            if (x = 2) or (x = 1) or (x = 0) then
+            begin
+                    crladder := 1;
+            end
+            else
+            begin
+                    for currentfloor := previous - 1 downto 1 do
+                    begin
+                        floors := floors + crladder(x - currentfloor, currentfloor);
+                    end;
+                    crladder := floors;
+            end;
 end;
 
 begin
-	readln(blocks);
-	writeln('Блоков: ', blocks);
-	floors := 0;
-	answer := crladder(blocks);
-	writeln('Ответ: ', answer);
+        readln(blocks);
+        writeln(crladder(blocks, blocks + 1));
 end.
