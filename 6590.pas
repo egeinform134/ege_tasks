6b7d6bd491e554405b0a309233e34394591acc80
Dmitry Luschan <dluschan@gmail.com>
2016-06-11 11:27:06 +0300

    fix

--- 6590.pas
+++ 6590.pas
@@ -1,18 +1,18 @@
 program sixfiveninezero;
 var
     i, j, t, pair, amount: integer;
-    votes, order: array [1..10] of integer;
+    votes, pairs: array [1..10] of integer;
 begin
     for i := 1 to 10 do
     begin
         votes[i] := 0;
-        order[i] := i;
+        pairs[i] := i;
     end;
 
     readln(amount);
     for i := 1 to amount do
     begin
-        readln(pair);
+        read(pair);
         inc(votes[pair]);
     end;
 
@@ -24,11 +24,16 @@ begin
         for j := i downto 2 do
             if (votes[j] < votes[j-1]) then
             begin
-                t := order[j];
-                order[j] := order[j-1];
-                order[j-1] := t;
+                t := votes[j];
+                votes[j] := votes[j-1];
+                votes[j-1] := t;
+
+                t := pairs[j];
+                pairs[j] := pairs[j-1];
+                pairs[j-1] := t;
             end;
 
     for i := 1 to 10 do
-        writeln('Пара ', i, 'на ', order[i], ' месте');
+        if votes[i] <> 0 then
+            writeln(pairs[i], ' ', votes[i]);
 end.
